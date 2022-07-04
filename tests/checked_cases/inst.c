93783ad279ffda3a652f72a66e9cbace83db0a62
arunkumarbhattar <bhattar1@purdue.edu>
2022-07-04 04:29:22 -0400

    Changing Name from TT to CheckMate

--- tests/checked_cases/inst.c
+++ tests/checked_cases/inst.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 
-struct costMatrixRow { int x; int y; };
+struct costMatrixRow {
+  int x;
+  int y;
+};
 void foo(int channelNets) {
-  _Array_ptr<struct costMatrixRow> costMatrix = malloc<struct costMatrixRow>((channelNets+1) * sizeof(struct costMatrixRow));
+  _Array_ptr<struct costMatrixRow> costMatrix = malloc<struct costMatrixRow>(
+      (channelNets + 1) * sizeof(struct costMatrixRow));
   int i = 0;
   for (; i < channelNets; i++) {
     costMatrix[i].x = 0;
